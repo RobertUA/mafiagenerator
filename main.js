378008619b1d5fb504838ede0b1dd388f6a1a3ea
Роберт Шевцов <55320224+RobertUA@users.noreply.github.com>
2021-05-01 23:03:20 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ function Hide()
     if(document.getElementById("list").style.display == "none") 
     {
         document.getElementById("list").style.display = "block";
-        document.getElementById("hide").textContent = "СПРЯТАТЬ"
+        document.getElementById("roles").style.display = "none";
+        document.getElementById("hide").textContent = "РОЛИ"
         localStorage.setItem("hide", 0);
     }
     else
     {
         document.getElementById("list").style.display = "none";
-        document.getElementById("hide").textContent = "ПОКАЗАТЬ"
+        document.getElementById("roles").style.display = "grid";
+        document.getElementById("hide").textContent = "ИГРОКИ"
         localStorage.setItem("hide", 1);
     }
 }
@@ -27,12 +29,17 @@ function Addp()
         document.getElementById("mafc").value = temp;
     }
     if(Number(document.getElementById("count").value)<2 || Number(document.getElementById("mafc").value)<1) return 1;
+    if(Number(document.getElementById("count").value)>299 || Number(document.getElementById("mafc").value)>149)
+    {
+        alert("Ти шо, дурак?");
+        return 1;
+    }
 
     if(localStorage.getItem('hide')==0 && document.getElementById("list").style.display=='none') 
     {
         document.getElementById("list").style.display = "block";
-        document.getElementById("hide").textContent = "СПРЯТАТЬ"
-
+        document.getElementById("hide").textContent = "РОЛИ";
+        document.getElementById("roles").style.display = "none";
     }
 
     document.getElementById("btn").disabled = true;
@@ -117,11 +124,27 @@ function Addp()
 
 function load()
 {
-    if(localStorage.length<=1) 
+    if(localStorage.getItem("values")!=undefined)
     {
-        alert("Была обновОчка.\n - Данные сохраняются.\n - У ролей есть цвета.\n - Кнопка \"СПРЯТАТЬ/ПОКАЗАТЬ\".");
+        let str = localStorage.getItem("values");
+        if(str[0]=='1') document.getElementById("1").checked=true;
+        if(str[1]=='1') document.getElementById("2").checked=true;
+        if(str[2]=='1') document.getElementById("3").checked=true;
+        if(str[3]=='1') document.getElementById("4").checked=true;
+        if(str[4]=='1') document.getElementById("5").checked=true;
+        if(str[5]=='1') document.getElementById("6").checked=true;
+        if(str[6]=='1') document.getElementById("7").checked=true;
+        if(str[7]=='1') document.getElementById("8").checked=true;
+    }
+    if(localStorage.getItem("mafcount")!=undefined) document.getElementById("mafc").value = localStorage.getItem("mafcount");
+    if(localStorage.getItem("playerscount")!=undefined) document.getElementById("count").value = localStorage.getItem("playerscount");
+
+    if(localStorage.length<=0) alert("Была обновОчка.\n - Данные сохраняются.\n - У ролей есть цвета.\n - Переключение вида \"ИГРОКИ/РОЛИ\".");
+
+    if(localStorage.length<=4) 
+    {
         localStorage.setItem('hide', 0);
-        document.getElementById("hide").textContent = "ПОКАЗАТЬ"
+        document.getElementById("hide").textContent = "ИГРОКИ"
         document.getElementById("list").style.display = "none";
         document.getElementById("hide").disabled = true;
         return 1;
@@ -130,25 +153,16 @@ function load()
     if(localStorage.getItem('hide') == 0) 
     {
         document.getElementById("list").style.display = "block";
-        document.getElementById("hide").textContent = "СПРЯТАТЬ"
+        document.getElementById("roles").style.display = "none";
+        document.getElementById("hide").textContent = "РОЛИ"
     }
     else 
     {
         document.getElementById("list").style.display = "none";
-        document.getElementById("hide").textContent = "ПОКАЗАТЬ"
+        document.getElementById("roles").style.display = "grid";
+        document.getElementById("hide").textContent = "ИГРОКИ"
     }
 
-    let str = localStorage.getItem("values");
-    if(str[0]=='1') document.getElementById("1").checked=true;
-    if(str[1]=='1') document.getElementById("2").checked=true;
-    if(str[2]=='1') document.getElementById("3").checked=true;
-    if(str[3]=='1') document.getElementById("4").checked=true;
-    if(str[4]=='1') document.getElementById("5").checked=true;
-    if(str[5]=='1') document.getElementById("6").checked=true;
-    if(str[6]=='1') document.getElementById("7").checked=true;
-    if(str[7]=='1') document.getElementById("8").checked=true;
-    document.getElementById("mafc").value = localStorage.getItem("mafcount");
-    document.getElementById("count").value = localStorage.getItem("playerscount");
     let count = 0;
     for(let i=0; i<document.getElementById("count").value;i++)
     {
